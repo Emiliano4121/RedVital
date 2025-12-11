d452d2804d54e097eda153e8787cbc241b54a314
Emiliano4121 <maldonadoemiliano41@gmail.com>
2025-12-11 02:00:33 -0600

    se agrego el home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { Search } from './pages/Busqueda/search/search';
+import { Home } from './pages/Inicio/home/home';
 
 export const routes: Routes = [
+
+    { path: '', component: Home, title: 'RedVital - Inicio' },
     {path: 'busqueda', component: Search},
     {path: '', redirectTo: 'busqueda', pathMatch: 'full' }
 ];
