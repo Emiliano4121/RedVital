e58ea1425efb37909a1a8c5f2e10090cd97a0847
Emiliano4121 <maldonadoemiliano41@gmail.com>
2026-04-15 19:59:01 -0600

    agregamos funciones a los botones

--- src/app/pages/Inicio/home/home.ts
+++ src/app/pages/Inicio/home/home.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
 
+  constructor(private router: Router) {}
+
+  irPaciente() {
+    this.router.navigate(['/dashboard/paciente']);
+  }
+
+  irEnfermero() {
+    this.router.navigate(['/dashboard/enfermero']);
+  }
 }
